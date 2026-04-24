f395d7269ad0067a6eccc61c9aaadc5084cc9a87
Michael Froschauer <mi.froschauer@gmx.at>
2026-04-24 15:25:27 +0200

    Add icons and make it a PWA

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+
+// This cache version must be updated if the cached files are changed,
+// otherwise the old files will be used and the app might break.
+// The old cache will be deleted on activation of the new service worker.
+const CACHE_NAME = "zaehlmeister-pwa-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
